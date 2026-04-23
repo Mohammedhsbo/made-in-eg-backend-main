88ee9cb87bfc50cccc2f315d60758dfe6ca5a663
muhamedhsbo <mohaamedhassabo26@gmail.com>
2026-04-23 19:52:24 +0200

    error fixing of mailer

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ connectDB();
 app.use(helmet());
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://localhost:5174'
+  'http://localhost:5174',
+  'https://made-in-egypt-one.vercel.app'
 ];
 
 app.use(
